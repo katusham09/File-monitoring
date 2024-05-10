e6223e51ec3c1fbfb16efca69745ad085e03eeba
Ekaterina <katusham0912@gmail.com>
2024-05-10 21:48:54 +0700

    Написан класс Manager
    в котором происходит слежение за файлами

--- fileInfo.h
+++ fileInfo.h
@@ -14,6 +14,9 @@ public:
     FileInfo(const QString& filePath);//конструктор
     QString getPath() const;//получение пути к файлу
     int getSize() const;//получение размера файла
+    bool getExistence() const;//получение флага существования файла
+    void setExistence(bool existence);//установка существования файла
+    void setSize(int newSize);//установка размера файла
 };
 
 #endif // FILEINFO_H
--- manager.h
+++ manager.h
@@ -1,11 +1,29 @@
 #ifndef MANAGER_H
 #define MANAGER_H
+#include <QObject>
+#include <QMap>
+#include <QTimer>
+#include "fileInfo.h"
+#include "logger.h"
 
-
-class manager
+class Manager : public QObject
 {
+    Q_OBJECT // Макрос для поддержки сигналов и слотов в Qt
+
 public:
-    manager();
+    Manager(); // Конструктор класса
+    void addFile(const QString& path); // Метод для добавления файла в список отслеживаемых файлов
+    void deleteFile(const QString& path); // Метод для удаления файла из списка отслеживаемых файлов
+    void checkChanges(); // Метод для проверки изменений в отслеживаемых файлах
+
+private:
+    QVector<FileInfo> files; // Вектор для хранения информации о файлах
+    QTimer timer; // Таймер для периодической проверки изменений в файлах
+signals:
+    void fileSizeChanged(const QString& path, int size); // Сигнал об изменении размера файла
+    void fileCreated(const QString& path, int size); // Сигнал о создании файла
+    void fileDeleted(const QString& path); // Сигнал об удалении файла
 };
+
 
 #endif // MANAGER_H
